5e63f8b18c9136583b946391f37530388365ffaf
ArloBedard <2345955@csfoy.ca>
2025-05-18 02:10:40 -0400

    Gestion des permission d'accès aux différentes routes

---
+++ src/views/TesterBugsView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">import { computed, watch } from 'vue'
+import { useProfileStore } from '../stores/profileStore'
+import { useRouter } from 'vue-router'
+
+const profileStore = useProfileStore()
+
+const router = useRouter()
+
+const role = computed(() => profileStore.role)
+
+if (role.value !== 'tester') {
+    router.push({ name: 'LeadBugs' })
+}
+</script>
+
+<template></template>
+
+<style></style>
